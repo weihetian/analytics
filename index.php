5bf1c4b80b5b56689de2bb1725dfbe4edd7b085e
Scott Tian <santanasaki@Scott.(none)>
2015-06-01 13:36:06 -0400

    html added ;

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="google-site-verification" content="tp-i_OBftNvhRzM8ME25CS8VGzGZW0M_oeizPYR5lcU" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+          <meta name="viewport" content="width=device-width">
+
+
+  	    <title>Analytics</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="js/jquery.transit.min.js"></script>
+		 <link href="css/_login.css" rel="stylesheet">
+
+</head>
+<body>
+	<div class='container'>
+		<div class='row center_input'>
+			<div class='col-md-3 col-md-offset-4'>
+				<input class='form-control' placeholder='campaign name' />
+			</div>
+			<div class='col-md-1'>
+				<input type="submit" value="submit" class="blue-flat-button" >
+			</div>
+			
+			
+		</div>
+		
+	</div>
+</body>
+
+</html>
