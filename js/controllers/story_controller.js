4e70ca6a76eb8a89b403f033cc3e624a3354f7f1
Scott Tian <santanasaki@Scott.(none)>
2015-06-15 11:43:02 -0400

    update

--- js/controllers/story_controller.js
+++ js/controllers/story_controller.js
@@ -7,23 +7,23 @@
 			
 			var dataString='campaignid='+$scope.current_campaign_id+"&content="+$scope.story;
 		alert(dataString);
-			// $.ajax({
-	// 			type: "POST",
-	// 			url: "data_access/pull_mile_data.php",
-	// 			datatype: "html",
-	// 			data: dataString,
-	// 			success: function(data) {
-	// 						//var campaigns = $.parseJSON(data);
-	// 				alert(data);
-	// 				//$scope.campaigns = campaigns;
-	// 				//$scope.$digest();
-	// 				//	alert(data);
-	// 				// var results = $.parseJSON(data);
-	// 				// $scope.posts = results;
-	// 				// $scope.$digest();
-	// 				// alert(data);
-	// 			}
-	// 		})
+			$.ajax({
+				type: "POST",
+				url: "data_access/pull_mile_data.php",
+				datatype: "html",
+				data: dataString,
+				success: function(data) {
+							//var campaigns = $.parseJSON(data);
+					alert(data);
+					//$scope.campaigns = campaigns;
+					//$scope.$digest();
+					//	alert(data);
+					// var results = $.parseJSON(data);
+					// $scope.posts = results;
+					// $scope.$digest();
+					// alert(data);
+				}
+			})
 		}
 		
 		
